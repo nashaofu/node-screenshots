629f8ae2b20a7386559c1b5cc026af0dbc4c6dde
nashaofu <diaocheng@outlook.com>
2023-04-19 09:10:29 +0800

    chore: 修改编译脚本 (#26)
    * chore: 修改编译脚本
    
    * chore: 添加 universal mac，修改 CI 脚本
    
    * docs: 修改 README
    
    * chore: 去掉 arm 架构编译脚本
    
    * fix: 升级脚本
    
    * chore: 修改dockerfile

--- .devcontainer/docker-init.sh
+++ .devcontainer/docker-init.sh
@@ -8,17 +8,17 @@ DISPLAY_HEIGHT="${DISPLAY_HEIGHT:-720}"
 
 # 后台运行程序
 startInBackgroundIfNotRunning() {
-    echo "Starting $1."
-    if ! pidof $1 >/dev/null; then
-        $@ &
-        while ! pidof $1 >/dev/null; do
-            echo "Waiting $1 start"
-            sleep 1
-        done
-        echo "$1 started."
-    else
-        echo "$1 is already running."
-    fi
+  echo "Starting $1."
+  if ! pidof $1 >/dev/null; then
+    $@ &
+    while ! pidof $1 >/dev/null; do
+      echo "Waiting $1 start"
+      sleep 1
+    done
+    echo "$1 started."
+  else
+    echo "$1 is already running."
+  fi
 }
 
 startInBackgroundIfNotRunning Xvfb ${DISPLAY} -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x24 -dpi 96 -listen tcp -ac
---
+++ npm/darwin-universal/README.md
@@ -0,0 +1,3 @@
+# `node-screenshots-darwin-universal`
+
+This is the **universal-apple-darwin** binary for `node-screenshots`
