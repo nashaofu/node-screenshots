ff79892329d3a4420738cb895028821504897d04
nashaofu <19303058+nashaofu@users.noreply.github.com>
2025-12-14 09:25:00 +0800

    fix: s

--- scripts/x86_64-unknown-linux-musl.sh
+++ scripts/x86_64-unknown-linux-musl.sh
@@ -4,7 +4,7 @@ set -e
 apk update
 apk add curl pkgconf clang-dev libxcb-dev libxrandr-dev dbus-dev pipewire-dev
 
-curl -fsSL https://fnm.vercel.app/install | bash
+curl -fsSL https://fnm.vercel.app/install | ash
 source $HOME/.bashrc
 fnm install 24
 fnm use 24
