b97bc3383a86798aedf069134e769802963831dd
nashaofu <19303058+nashaofu@users.noreply.github.com>
2025-12-14 09:21:45 +0800

    fix: 修复报错

--- scripts/x86_64-unknown-linux-gnu.sh
+++ scripts/x86_64-unknown-linux-gnu.sh
@@ -2,12 +2,12 @@
 
 set -e
 apt-get update
-apt-get install -y curl build-essential pkg-config libclang-dev libxcb1-dev libxrandr-dev libdbus-1-dev libpipewire-0.3-dev
+apt-get install -y curl pkg-config libclang-dev libxcb1-dev libxrandr-dev libdbus-1-dev libpipewire-0.3-dev
 
 curl -fsSL https://fnm.vercel.app/install | bash
 source $HOME/.bashrc
-fnm install 16
-fnm use 16
+fnm install 24
+fnm use 24
 npm install -g yarn
 
 yarn build --target x86_64-unknown-linux-gnu
--- scripts/x86_64-unknown-linux-musl.sh
+++ scripts/x86_64-unknown-linux-musl.sh
@@ -2,12 +2,12 @@
 
 set -e
 apk update
-apk add curl build-base pkgconf clang-dev libxcb-dev libxrandr-dev dbus-dev pipewire-dev
+apk add curl pkgconf clang-dev libxcb-dev libxrandr-dev dbus-dev pipewire-dev
 
 curl -fsSL https://fnm.vercel.app/install | bash
 source $HOME/.bashrc
-fnm install 16
-fnm use 16
+fnm install 24
+fnm use 24
 npm install -g yarn
 
 yarn build --target x86_64-unknown-linux-musl
