be959276adf41b5efa0370118c7a9ed601a8ba83
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-01-23 14:47:53 +0800

    feat: add LoongArch support

---
+++ scripts/loongarch64-unknown-linux-gnu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Updating package lists and installing dependencies..."
+apt-get update
+apt-get install -y \
+  curl \
+  build-essential \
+  pkg-config \
+  libclang-dev \
+  libxcb1-dev \
+  libxrandr-dev \
+  libdbus-1-dev \
+  libpipewire-0.3-dev
+
+echo "Installing rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+rustup target add loongarch64-unknown-linux-gnu
+
+yarn build --target loongarch64-unknown-linux-gnu
