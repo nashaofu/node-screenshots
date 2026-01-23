3679f0702774b5571fcf8ee5ad69214ae42bc9db
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-01-23 19:46:11 +0800

    fix: use the last usable snapshot for loongarch64 for now
    With latest version:
    ```
    The following packages have unmet dependencies:
     libasound2-data : Breaks: libasound2t64 (< 1.2.15.3-1)
    E: Unable to correct problems, you have held broken packages.
    E: The following information from --solver 3.0 may provide additional context:
       Unable to satisfy dependencies. Reached two conflicting decisions:
       1. libasound2-data:loong64 is not selected for install because:
          1. libpipewire-0.3-dev:loong64=1.4.9-1 is selected for install
          2. libpipewire-0.3-dev:loong64 Depends libpipewire-0.3-0t64 (= 1.4.9-1)
          3. libpipewire-0.3-0t64:loong64 Depends libspa-0.2-modules (= 1.4.9-1)
          4. libspa-0.2-modules:loong64 Depends libasound2t64 (>= 1.2.10)
          5. libasound2-data:loong64 Breaks libasound2t64 (< 1.2.15.3-1)
       2. libasound2-data:loong64 is selected for install because:
          1. libpipewire-0.3-dev:loong64=1.4.9-1 is selected for install as above
          1-4. libasound2t64:loong64=1.2.15-2 is selected for install as above
          5. libasound2t64:loong64 Depends libasound2-data (>= 1.2.15-2)
    ```

--- scripts/loongarch64-unknown-linux-gnu.sh
+++ scripts/loongarch64-unknown-linux-gnu.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+echo "Use the last snapshot without dependency issues for now..."
+rm -f /etc/apt/sources.list.d/*
+cat << EOF | tee /etc/apt/sources.list.d/debian-ports.list
+deb http://snapshot.debian.org/archive/debian-ports/20260108T013037Z/ sid main
+EOF
+( echo 'quiet "true";'; \
+  echo 'APT::Get::Assume-Yes "true";'; \
+  echo 'APT::Install-Recommends "false";'; \
+  echo 'Acquire::Check-Valid-Until "false";'; \
+  echo 'Acquire::Retries "5";'; \
+) > /etc/apt/apt.conf.d/99snapshot-repos
+
 echo "Updating package lists and installing dependencies..."
 apt-get update
 apt-get install -y \
@@ -19,5 +31,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
 rustup target add loongarch64-unknown-linux-gnu
+
+export CARGO_TARGET_LOONGARCH64_UNKNOWN_LINUX_GNU_LINKER=gcc
 
 yarn build --target loongarch64-unknown-linux-gnu
